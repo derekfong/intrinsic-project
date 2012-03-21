093aa1f3753015c2486fa09c8ec1988ce55974ad
Kevin Mann <kma50@sfu.ca>
2012-03-20 21:44:05 -0700

    Fixed HTML/CSS, added histogram

--- SOL/Gradebook/views.py
+++ SOL/Gradebook/views.py
@@ -7,6 +7,7 @@ from Gradebook.models import Grade, GradeComment
 from Instructor.models import Activity
 from Student.views import instAccess, studentAccess, getInsts, getTas, getEnrolled
 from django.db.models import Avg, Max, Min, Count, StdDev
+from array import *
 #from chartit import DataPool, Chart
 
 # Create your views here.
@@ -34,7 +35,35 @@ def viewGrade(request, department, class_number, year, semester, section, aid):
 	activity = percentOne(tmpActivity)
 	comments = GradeComment.objects.filter(gid=activity.gid)
 	
-	sortGrade = Grade.objects.filter(aid=aid).order_by('mark')
+	tmpGrade = Grade.objects.filter(aid=aid).order_by('mark')
+	sortGrade = percentAll(tmpGrade)
+	#barChart = [0,0,0,0,0,0,0,0,0,0,0]
+	barChart = [6,3,8,6,9,4,7,4,9,7,1]
+	for aGrade in sortGrade:
+		if aGrade.percent < 10:
+			barChart[0] += 1
+		elif aGrade.percent < 20:
+			barChart[1] += 1
+		elif aGrade.percent < 30:
+			barChart[2] += 1
+		elif aGrade.percent < 40:
+			barChart[3] += 1
+		elif aGrade.percent < 50:
+			barChart[4] += 1
+		elif aGrade.percent < 60:
+			barChart[5] += 1
+		elif aGrade.percent < 70:
+			barChart[6] += 1
+		elif aGrade.percent < 80:
+			barChart[7] += 1
+		elif aGrade.percent < 90:
+			barChart[8] += 1
+		elif aGrade.percent < 100:
+			barChart[9] += 1
+		else:
+			barChart[10] += 1
+	#barChart = [a,b,k,d,e,f,g,h,i,j]
+	
 	if sortGrade.count() % 2 == 1:
 		median = sortGrade[(sortGrade.count()-1)/2].mark
 	else:
@@ -48,8 +77,9 @@ def viewGrade(request, department, class_number, year, semester, section, aid):
 	accessToInst = instAccess(getInsts(class_id), getTas(class_id), user)
 	accessToStudent = studentAccess(getEnrolled(class_id), user) or accessToInst
 	
-	return render_to_response('gradebook/viewGrade.html', {'class': c, 'accessToStudent': accessToStudent, 'activity': activity, 'comments': comments, 'stats': stats, 
-'median': median },
+	#context = {'class': c, 'accessToStudent': accessToStudent, 'activity': activity, 'comments': comments, 'stats': stats, 'median': median, 'barChart': barChart, }
+	
+	return render_to_response('gradebook/viewGrade.html', {'class': c, 'accessToStudent': accessToStudent, 'activity': activity, 'comments': comments, 'stats': stats, 'median': median, 'barChart': barChart, },
 		context_instance=RequestContext(request))
 		
 
