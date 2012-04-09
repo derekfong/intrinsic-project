b4b7902fda9104787a4df1569ebc1e1fab759d03
Kevin Mann <kma50@sfu.ca>
2012-04-09 15:22:33 -0700

    Commented Student View

--- SOL/Student/views.py
+++ SOL/Student/views.py
@@ -8,7 +8,6 @@ from Main.views import currentSemester
 from Student.models import Submission, QuizAttempt, QuizResult
 from Gradebook.models import Grade
 from Student.forms import SubmissionForm
-#from reportlab.pdfgen import canvas
 from django.http import HttpResponse, HttpResponseRedirect
 import datetime, os
 from datetime import timedelta
@@ -17,16 +16,17 @@ import cStringIO as StringIO
 import cgi
 from django.template.loader import get_template
 	
-# Create your views here.
+#Initial view for students
 def index(request, department, class_number, year, semester, section):
 	# gets corresponding classes to display on the website
 	user = request.user
 	c = getClassObject(department, class_number, year, semester, section, user)
 	
-	nextWeek = datetime.datetime.now()+timedelta(days=14)
-
+	#Gets all the activites coming up in the next 7 days 
+	nextWeek = datetime.datetime.now()+timedelta(days=7)
 	upcomingActivity = Activity.objects.filter(due_date__lte = nextWeek, due_date__gte = datetime.datetime.now(), cid=c.cid).order_by('due_date')
 	
+	#Check if greeting exists, otherwise display generic welcome message
 	try: 
 		greeting = Greeting.objects.get(cid=c.cid).message
 	except Greeting.DoesNotExist:
@@ -38,13 +38,15 @@ def index(request, department, class_number, year, semester, section):
 	
 	return render_to_response('student/index.html', content,
 		context_instance=RequestContext(request))
-		
+
+#View to display syllabus		
 def syllabus(request, department, class_number, year, semester, section):
 	# grab corresponding syllabus for that class
 	user = request.user
 	c = getClassObject(department, class_number, year, semester, section, user)
 		
 	message = ''
+	#Check if a syllabus has been created
 	try:
 		syllabus = CourseContent.objects.get(cid = c.cid)
 	except CourseContent.DoesNotExist:
@@ -57,12 +59,14 @@ def syllabus(request, department, class_number, year, semester, section):
 	
 	return render_to_response('student/syllabus.html', content, 
 		context_instance=RequestContext(request))
-		
+
+#View to download Syllabus as PDF		
 def downloadSyllabus(request, department, class_number, year, semester, section):
 	user = request.user
 	c = getClassObject(department, class_number, year, semester, section, user)
 
 	message = ''
+	#Check if syllabus exists
 	try:
 		syllabus = CourseContent.objects.get(cid = c.cid)
 	except CourseContent.DoesNotExist:
@@ -73,20 +77,25 @@ def downloadSyllabus(request, department, class_number, year, semester, section)
 	content['syllabus'] = syllabus
 	content['message'] = message
 
+	#Gets the template to use to create the PDF
 	template_src= 'student/syllabusPDF.html'
-	context_dict = content
 	template = get_template(template_src)
+	#Gets the context to send to the template
+	context_dict = content
 	context = Context(context_dict)
+	
+	#Convert the template from HTML into PDF
 	html  = template.render(context)
 	result = StringIO.StringIO()
 	pdf = pisa.pisaDocument(StringIO.StringIO(html.encode("UTF-16")), result)
+	
+	#Send HTTP Response with the PDF
 	if not pdf.err:
 		return HttpResponse(result.getvalue(), mimetype='application/pdf')
 	return HttpResponse('We had some errors<pre>%s</pre>' % cgi.escape(html))
 
+#Displays slides to students
 def slides(request, department, class_number, year, semester, section):
-	# slides are lecture notes 
-	# student has to find corresponding sildes for that course...
 	user = request.user
 	c = getClassObject(department, class_number, year, semester, section, user)
 		
@@ -98,8 +107,8 @@ def slides(request, department, class_number, year, semester, section):
 	return render_to_response('student/slides.html', content, 
 		context_instance=RequestContext(request))
 	
+#Displayed Activities and allows students to submit assignments
 def activities(request, department, class_number, year, semester, section):
-	# activities are assignments, exams, midterms, etc	
 	user = request.user
 	c = getClassObject(department, class_number, year, semester, section, user)
 	
@@ -113,7 +122,7 @@ def activities(request, department, class_number, year, semester, section):
 	return render_to_response('student/activities.html', content, 
 		context_instance=RequestContext(request))
 
-# View that allows a student to submit a file for an activity.
+#View that allows a student to submit a file for an activity.
 def activities_submit(request, department, class_number, year, semester, section, aid):
 	class_id = Course.objects.get(department=department, class_number=class_number, year=year, semester=semester, section=section).cid
 	c = get_object_or_404(Course, pk=class_id)
@@ -158,6 +167,7 @@ def activities_submit(request, department, class_number, year, semester, section
 	return render_to_response('student/submission.html', content, 
 		context_instance=RequestContext(request))
 
+#Displays Quizzes available to student
 def quiz(request, department, class_number, year, semester, section):
 	user = request.user
 	c = getClassObject(department, class_number, year, semester, section, user)
@@ -165,7 +175,10 @@ def quiz(request, department, class_number, year, semester, section):
 	now = datetime.datetime.now()
 	quizzes = Quiz.objects.filter(cid=c.cid)
 	for quiz in quizzes:
+		#Number of attempts on quiz for student
 		quiz.attempts = QuizAttempt.objects.filter(qid=quiz.id, uid=user.id).count()
+		
+		#Quiz is current if within start and end date
 		if quiz.start_date <= now and quiz.end_date >= now:
 			quiz.current = 1
 		else:
@@ -180,35 +193,44 @@ def quiz(request, department, class_number, year, semester, section):
 def quizTake(request, department, class_number, year, semester, section, qid):
 	user = request.user
 	c = getClassObject(department, class_number, year, semester, section, user)
-	
 	q = get_object_or_404(Quiz, pk=qid)
+	
 	now = datetime.datetime.now()
 	
+	#Quiz is current if within start and end date
 	if q.start_date <= now and q.end_date >= now:
 		q.current = 1
 	else:
 		q.current = 0
 	
+	#Number of attempts on quiz for student
 	attempts = QuizAttempt.objects.filter(qid=q.id, uid=user.id)
 	
+	#Gets all quiz questions
 	questions = QuizQuestion.objects.filter(qid=q.id)
 	
 	message = ''
+	#Grade the quiz and return the results to the students
 	if request.method == "POST":
 		finalMark = 0
+		#Make sure student has answered all questions
 		try: 
 			answers = []
 			for question in questions:
+				#Gets the students guess for the question
 				question.guess = int(request.POST.__getitem__(str(question.id)))
 				answer = question.answer
+				#Final Mark is incremented if student gets it right
 				if question.guess == answer:
 					question.result = 1
 					finalMark += 1
 				else:
 					question.result = 0
+			#Commits a quiz attempt to database
 			attempt = QuizAttempt(qid=q, uid=user.userprofile, time=datetime.datetime.now(), result=finalMark)
 			attempt.save()
 
+			#Commits a quiz result for each question answered to database
 			for question in questions:
 				qObj = QuizQuestion.objects.get(id=question.id)
 				QuizResult(attempt=attempt, question=qObj, guess=question.guess).save()
@@ -235,15 +257,20 @@ def quizTake(request, department, class_number, year, semester, section, qid):
 	return render_to_response('student/quizTake.html', content, 
 		context_instance=RequestContext(request))
 		
+#Shows the results for all attempts at a quiz
 def quizResults(request, department, class_number, year, semester, section, qid):
 	user = request.user
 	c = getClassObject(department, class_number, year, semester, section, user)
 	q = get_object_or_404(Quiz, pk=qid)
 	
+	#Gets all the student's attempts at the quiz
 	attempts = QuizAttempt.objects.filter(qid=q.id, uid=user.id)
 	for attempt in attempts:
+		#Gets all the student's results for each attempt
 		attempt.results = QuizResult.objects.filter(attempt=attempt.id)
+		#Gets the max mark for quiz
 		attempt.out_of = attempt.results.count()
+		#Check if student got each question right
 		for result in attempt.results:
 			if result.guess == result.question.answer:
 				result.result = 1
@@ -257,12 +284,12 @@ def quizResults(request, department, class_number, year, semester, section, qid)
 	return render_to_response('student/quizAttempts.html', content, 
 		context_instance=RequestContext(request))	
 
+#Displays all the announcements
 def announcements(request, department, class_number, year, semester, section):
-	# instructors can leave annoucement messages on the home page for students to view 
-	# they log in
 	user = request.user
 	c = getClassObject(department, class_number, year, semester, section, user)
 
+	#Get all announcements for class
 	announcements = Announcement.objects.filter(cid=c.cid).order_by('-date_posted')
 
 	content = getContent(c, user)
@@ -271,6 +298,7 @@ def announcements(request, department, class_number, year, semester, section):
 	return render_to_response('student/announcements.html', content, 
 		context_instance=RequestContext(request))			
 		
+#Gets the 3 newest announcements for a given course and truncate them if too long
 def getAnnouncements(class_id):
 	latestAnnouncements = Announcement.objects.filter(cid=class_id).order_by('-date_posted')[:3]
 	for announcement in latestAnnouncements:
@@ -285,14 +313,14 @@ def getAnnouncements(class_id):
 			announcement.content = announcement.content
 	return latestAnnouncements	
 	
+#Check to make sure an activity isn't past the due date
 def pastDue(activity):
 	if datetime.datetime.now() > activity.due_date:
 		return 1
 	else:
 		return 0
 
-# sees if there is permission as instructor/marker
-# Diff features depending on permission.
+#Check if there is permission as instructor/marker
 def instAccess(instructors, tas, user):
 	for ta in tas:
 		if user.id == ta.user.id:
@@ -302,8 +330,7 @@ def instAccess(instructors, tas, user):
 			return 1
 	return 0
 
-# sees if there is permission as student.
-# Diff features depending on permission.
+#Check if there is permission as student.
 def studentAccess(students, user):
 	for student in students:
 		if user.id == student.user.id:
@@ -311,7 +338,7 @@ def studentAccess(students, user):
 	return 0
 
 
-# SETTERS AND GETTERS from models
+#Gets Instructors, TAs, Students, Enrolled Accounts
 def getInsts(class_id):
 	return UserProfile.objects.filter(classlist__cid=class_id, classlist__is_instructor=1)
 
@@ -323,11 +350,13 @@ def getStudents(class_id):
 	
 def getEnrolled(class_id):
 	return UserProfile.objects.filter(classlist__cid=class_id)
-		
+
+#Get class URL		
 def getClassUrl(c):
 	classUrl = '/course/'+c.department+'/%s' %c.class_number+'/%s' %c.year+'/'+c.semester+'/'+c.section+'/'
 	return classUrl
 	
+#Check if a class is current (offered in current semester)
 def checkCurrent(c):
 	year = datetime.date.today().year
 	semester = currentSemester()
@@ -336,12 +365,14 @@ def checkCurrent(c):
 		return 1
 	else:
 		return 0
-	
+
+#Gets all current classes student is enrolled in	
 def getClassList(user):
 	year = datetime.date.today().year
 	semester = currentSemester()
 	return Course.objects.filter(classlist__uid=user.id, year=year, semester=semester).order_by('department', 'class_number')
-	
+
+#Get class object if it exists, otherwise return a 404	
 def getClassObject(department, class_number, year, semester, section, user):
 	try:
 		class_id = Course.objects.get(department=department, class_number=class_number, year=year, semester=semester, section=section).cid
@@ -350,8 +381,7 @@ def getClassObject(department, class_number, year, semester, section, user):
 
 	return get_object_or_404(Course, pk=class_id)
 
-
-# actually display content on the main page relating to the student user
+#Sets all the generic contexts to send to template
 def getContent(c, user):
 	instructors = getInsts(c.cid)
 	tas = getTas(c.cid)
